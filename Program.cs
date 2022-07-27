f08823853644708f3a43f566e36b5f3d1e7470a9
Kashaedov Vitaliy <Vitalickashaedov@mail.ru>
2022-07-27 21:10:04 +0300

    Zadacha66

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int temp = m;
+
+if (m > n) 
+{
+  m = n; 
+  n = temp;
+}
+
+PrintSum(m, n, temp = 0);
+
+void PrintSum(int m, int n, int sum)
+{
+  sum = sum + n;
+  if (n <= m)
+  {
+    Console.Write($"Сумма натуральных элементов = {sum}");
+    return;
+  }
+  PrintSum(m, n - 1, sum);
+}
